c35bc0591bca6ddb68351037015f13e22e35917e
Md Samiul <samiuljust2018@gmail.com>
2023-01-06 13:19:48 +0600

    update

--- controller/homePageController.js
+++ controller/homePageController.js
@@ -9,9 +9,9 @@ exports.getHomePage = async(req, res, next) => {
         } else if (req.user.role == '1') {
             return res.render('pages/teacher/teacher_homepage', { title: "Homepage-Teacher", user: req.user, posts })
         } else if (req.user.role == '3') {
-            return res.render('pages/admin/admin_homepage', { title: 'Admin Homepage', user: req.user })
+            return res.render('pages/admin/admin_homepage', { title: 'Admin Homepage', user: req.user, posts })
         } else if (req.user.role == '4') {
-            return res.render('pages/admin/editor_homepage', { title: 'Editor Homepage', user: req.user })
+            return res.render('pages/admin/editor_homepage', { title: 'Editor Homepage', user: req.user, posts })
         }
     } catch (e) {
         console.log(e)
